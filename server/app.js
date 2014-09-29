eee6a62e3216722d2e274e0390cee20adc7db4d5
Ryan Burke <ryan@2lemetry.com>
2014-09-29 12:50:00 -0600

    bug consuming env var

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ if (!process.env.THINGFABRIC_CONFIG) {
   throw new Error('`THINGFABRIC_CONFIG` missing!');
 }
 
-var config = JSON.parse('{'.concat(process.env.THINGFABRIC_CONFIG.split('=')[1]).concat('}'));
+var config = JSON.parse('{"'.concat(process.env.THINGFABRIC_CONFIG.concat('"}'));
 console.log(config);
 
 var path = require('path'),
