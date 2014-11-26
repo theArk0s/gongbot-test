d39124a7ed3e8e9f0e370aae8e130ead0d20d497
Kyle Roche <kyle@2lemetry.com>
2014-11-26 12:58:23 -0700

    added KC to DEN simulator

--- client/angular_components/directives/table/directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('com.2lemetry.heroku-demo.directives.table', []).directive('herokuDemoTable', [
-  function() {
-    return {
-      restrict: 'A',
-      scope: true,
-      templateUrl: 'client/angular_components/directives/table/directive.html'
-    };
-  }
-]);
