e004b240076c70187c412aa53a9e9fe2c9a24ad5
theArk0s <theark0s@gmail.com>
2015-02-26 11:28:14 -0800

    testing different entrypoint

--- server/app.js
+++ server/app.js
@@ -58,12 +58,13 @@ response.send(xml);
 function sms2mqtt(sms) {
   var encodedTopic, opts, request, url;
   request = require('request');
-  url = 'http://api.thingfabric.com/2';
- 
-  encodedTopic = encodeURIComponent('gvgxnrkdrpj9co1/RingGong');
+
+//  url = 'http://api.thingfabric.com/2';
+//  encodedTopic = encodeURIComponent('gvgxnrkdrpj9co1/RingGong');
  
   opts = {
-    uri: "" + url + "/account/domain/gvgxnrkdrpj9c1o/stuff/things/thing/GongButton/publish?topic=" + encodedTopic,
+//    uri: "" + url + "/account/domain/gvgxnrkdrpj9c1o/stuff/things/thing/GongButton/publish?topic=" + encodedTopic,
+    uri: "c3daf8f3-8683-44f0-b8b0-5a7b47d89fe8:7b867993-f81e-4b9a-ace9-934a1800e5c2@https://q.thingfabric.com/3/publish?topic=gvgxnrkdrpj9c1o/RingGong&clientid=c3daf8f3-8683-44f0-b8b0-5a7b47d89fe8&qos=0"
     method: "POST",
     json: {
       payload: "" + sms
